91c38f9a8cd8cd54c7978da3f3ce982b1d65b718
David McCann <david.a.mccann@gmail.com>
2011-10-06 11:40:18 +0300

    updated project names

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local, abort, run, lcd, cd, settings, sudo, env
 from fabric.contrib.console import confirm
 
-PROJECTS = ['mtraack', 'ureport', 'emis', 'status160']
+PROJECTS = ['mtrack', 'ureport', 'emis', 'status160']
 STANDARD_REPOS = [
    'django-eav',
    'rapidsms',
