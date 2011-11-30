7f5320015cca012447752a552f6f15a296d46b4a
miclovich <vicmiclovich@gmail.com>
2011-11-30 12:49:17 +0300

    remove unused...refactor for changes to server

--- fabfile.py
+++ fabfile.py
@@ -23,51 +23,6 @@ REPOS_WITH_SRC_NAME = [
     'rapidsms-xforms'
 ]
 
-
-# Schemamigrations specfic
-def migrate_app_schema(app, opts='init'):
-    #app-> `string` and name of app
-    #default option is init
-    #opts can take --auto, --add-field, etc.
-    run("./manage.py schemamigration %s --%s" % (app, opts))
-
-def run_init_schemamigration_project_apps(app_list):
-    """
-    migrate app; ./migrate <app_name> --init
-    """
-    for a in app_list:
-        migrate_app_schema(a)
-
-def run_auto_schemamigration_project_apps(app_list):
-    """
-    migrate app automatically
-    """
-    for a in app_list:
-        migrate_app_schema(a, opts='auto')
-
-# Migrations specific
-def migrate_app(app):
-    run("./manage.py migrate %s" % app)
-
-def run_auto_migrate_project_apps(app_list):
-    """
-    migrate apps with southmigration history
-    This function detects app's migration state automatically and
-     applies the necessary migration.
-    """
-    for a in app_list:
-        migrate_schema(a,)
-        run("./manage.py migrate %s --auto" % a)
-
-def run_migrate_project_apps(app_list):
-    """
-    Run migrate command on each migration
-    """
-    for a in app_list:
-        migrate_app(a)
-
-
-
 def deploy(project='all', dest='test', fix_owner='True', syncdb='False', south='False', south_initial='False', init_data='False',hash='False'):
     print "Fix owner is %s" % fix_owner
     if not dest in ['prod', 'test']:
@@ -126,6 +81,7 @@ def copy_db(project='all'):
         and not confirm("Project %s not in known projects (%s), proceed anyway?" % (project, PROJECTS)):
         abort('must specify a valid project: all or one of %s' % PROJECTS)
     projects = PROJECTS if project == 'all' else [project]
+
     for p in projects:
         sudo("pg_dump %s > /tmp/%s.pgsql" % (p, p), user="postgres")
         with settings(warn_only=True):
@@ -151,12 +107,6 @@ def pull_db(project='all', delete_local=True, from_local=False):
         local("sudo -u postgres psql %s < /tmp/%s.pgsql" % (p, p))
         if not delete_local == 'False':
             local("rm /tmp/%s.pgsql" % p)
-
-
-#TODO call to backup function for db's prior to a migration; only reinstate backed up DB on failure
-def backup_db(project='all'):
-    pass
-
 
 
 def add_all_submodules(project, dev=False):
